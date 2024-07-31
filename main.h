ddad5cee168da9b22f6c7570db4e9abca2b2ae60
surjith akshar <121660303+akshar7857914283@users.noreply.github.com>
2024-07-31 11:54:23 +0530

    Add files via upload

---
+++ main.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   main.h
+ * Author: aksha
+ *
+ * Created on 31 July, 2024, 11:50 AM
+ */
+
+#ifndef MAIN_H
+#define	MAIN_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MAIN_H */
+
